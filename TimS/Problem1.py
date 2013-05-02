2b65d2d8e573c5216141430e77c819dd20479195
Tim Skew <timskew@gmail.com>
2013-05-02 20:08:58 +0100

    Starting on Python problems

--- TimS/Problem1.py
+++ TimS/Problem1.py
@@ -1,3 +1,10 @@
 #Find the sum of all multiples of 3 or 5 below 1000
 #Tim Skew 2013-04-28
 
+total_sum = 0
+
+for i in range(1000):
+	if i % 3 == 0 or i % 5 == 0:
+		total_sum += i
+
+print (total_sum)
